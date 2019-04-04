e1b75011af34071880b7c93cd10d647b0ebc23e0
James Panetti <james@jameswebsite.net>
2019-04-04 14:37:15 -0500

    Added sleep commands, removed system.exit(1)

--- src/basicdemos/ChromeDriverDemo.java
+++ src/basicdemos/ChromeDriverDemo.java
@@ -1,8 +1,10 @@
 /**
- *  The ChromeDriverDemo script demonstrates what a basic Selenium chromedriver test looks like.
+ *  The JUnitDemo script demonstrates what a basic Selenium JUnit test looks like.
  *  This script will go to the DemoBlaze site, add a specific phone to the shopping cart,
  *  fill out an order form, then check out with its new purchase.
  *  (Don't worry -- the demo site is fake and no real purchases can be made!)
+ *  
+ *  This example was created using JUnit 4.
  *  
  *  @author James Panetti
  *  @version 1.0
@@ -12,6 +14,10 @@
 package basicdemos;
 
 import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -20,13 +26,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class ChromeDriverDemo {
+// Note that JUnit does not require a "main" function.
+public class JUnitDemo {
+	String baseURL = "http://www.demoblaze.com";
+	WebDriver driver = new ChromeDriver();
 
-	public static void main(String[] args) {
-		String baseURL = "http://www.demoblaze.com";
-		System.setProperty("webdriver.chrome.driver", "chromedriver");
-		WebDriver driver = new ChromeDriver();
+	// Everything within the @Before method executes before each test method.
+	@Before
+	public void setUp() throws Exception {
+		// System.setProperty is commented out because when executed through Taurus, Taurus will
+		// automatically download the required chromedriver. If the next line is not commented
+		// out, Taurus may hang indefinitely, confused by the script specifying the driver.
+		// System.setProperty("webdriver.chrome.driver", "chromedriver");
+		
+		// Implicit wait in place so that page will load before clicks are attempted:
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
 
+	// Note that the @Test notation is needed in order to execute this script through Taurus.
+	// Note that this demo only uses one @Test method.
+	@Test
+	public void test() {
 		driver.get(baseURL);
 		
 		// Verify that an element with the id 'nava' exists and contains the text 'PRODUCT STORE':
@@ -35,11 +55,7 @@ public class ChromeDriverDemo {
 		} catch (Exception e) {
 			System.out.println("Title text of \"Product Store\" not found on page.");
 			driver.close();
-			System.exit(1);
 		}
-		
-		// Implicit wait in place so that page will load before clicks are attempted:
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 		// Click the HTC One M9 link:
 		try {
@@ -47,7 +63,6 @@ public class ChromeDriverDemo {
 		} catch (Exception e) {
 			System.out.println("The link text \"HTC One M9\" could not be found.");
 			driver.close();
-			System.exit(1);
 		}
 		
 		// Verify the next shows "HTC One M9" in its h2 header:
@@ -56,16 +71,14 @@ public class ChromeDriverDemo {
 		} catch (Exception e) {
 			System.out.println("Text \"HTC One M9\" not found on next page.");
 			driver.close();
-			System.exit(1);
 		}
 		
-		// Add HTC One M9 to cart:
+		// Add the HTC One M9 to the cart:
 		try {
 			driver.findElement(By.linkText("Add to cart")).click();
 		} catch (Exception e) {
 			System.out.println("Could not find or click \"Add to cart\" button.");
 			driver.close();
-			System.exit(1);
 		}
 		
 		// Wait for then click the resulting pop-up message prompt:
@@ -75,16 +88,15 @@ public class ChromeDriverDemo {
 		} catch (Exception e) {
 			System.out.println("Could not find or click pop-up prompt.");
 			driver.close();
-			System.exit(1);
 		}
 		
 		// Click the Cart link:
 		try {
-			driver.findElement(By.xpath("//*[@id='cartur']")).click();
+			driver.findElement(By.id("cartur")).click();
+			Thread.sleep(2000);
 		} catch (Exception e) {
 			System.out.println("Could not find the Cart link.");
 			driver.close();
-			System.exit(1);
 		}
 
 		// Click the "Place Order" button:
@@ -93,43 +105,45 @@ public class ChromeDriverDemo {
 		} catch (Exception e) {
 			System.out.println("Could not find the \"Place Order\" button.");
 			driver.close();
-			System.exit(1);
 		}
 		
 		// Fill out then submit the order form:
 		try {
-			WebElement input_name = driver.findElement(By.xpath("//*[@id=\"name\"]"));
-			WebElement input_country = driver.findElement(By.xpath("//*[@id=\"country\"]"));
-			WebElement input_city = driver.findElement(By.xpath("//*[@id=\"city\"]"));
-			WebElement input_cc = driver.findElement(By.xpath("//*[@id=\"card\"]"));
-			WebElement input_month = driver.findElement(By.xpath("//*[@id=\"month\"]"));
-			WebElement input_year = driver.findElement(By.xpath("//*[@id=\"year\"]"));
+			WebElement input_name = driver.findElement(By.id("name"));
+			WebElement input_country = driver.findElement(By.id("country"));
+			WebElement input_city = driver.findElement(By.id("city"));
+			WebElement input_cc = driver.findElement(By.id("card"));
+			WebElement input_month = driver.findElement(By.id("month"));
+			WebElement input_year = driver.findElement(By.id("year"));
 			input_name.sendKeys("President Skroob");
 			input_country.sendKeys("Planet Spaceball");
 			input_city.sendKeys("Spaceball 1");
 			input_cc.sendKeys("12345");
 			input_month.sendKeys("June");
 			input_year.sendKeys("1987");
+			Thread.sleep(2000);
 			driver.findElement(By.xpath("//*[@id=\"orderModal\"]/div/div/div[3]/button[2]")).click();
 		} catch (Exception e) {
 			System.out.println("Could not fill out or submit the order form.");
 			driver.close();
-			System.exit(1);
 		}
 		
 		// Verify "Thank you for your purchase!" pop-up message, then click "OK":
 		try {
 			driver.findElement(By.xpath("/html/body/div[11]/h2[contains(text(), 'Thank you for your purchase!')]"));
+			Thread.sleep(2000);
 			driver.findElement(By.xpath("/html/body/div[11]/div[7]/div/button")).click();
 		} catch (Exception e) {
 			System.out.println("Could not find the confirmation message or the \"OK\" button.");
 			driver.close();
-			System.exit(1);
 		}
-		
+	}
+	
+	// Everything within the @After method executes after each test method.
+	@After
+	public void tearDown() throws Exception {
 		System.out.println("Test completed successfully!");
 		driver.close();
-		System.exit(0);
 	}
 
 }
